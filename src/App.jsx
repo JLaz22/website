f89de7164499594e812b2cc412e280a81bb82963
JLaz22 <lazcanojoel20@gmail.com>
2025-01-13 11:48:07 -0500

    update1.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './Pages/home'
 import { Page1 } from './Pages/page1'
 
