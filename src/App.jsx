d5547d040daf2438bc44cc0bb50d614eee593c1a
JLaz22 <lazcanojoel20@gmail.com>
2025-01-14 10:25:59 -0500

    update1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import NavigationBar from '../navbar'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/home'
 import { Page1 } from './Pages/page1'
 
 function App() {
 
   return(
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/page1" element={<Page1/>} />
+    <>
+     <Router>
+    <div>
+      <NavigationBar></NavigationBar>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/page1" element={<Page1/>} />
 
-      </Routes>
-    </Router>
+        </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
---
+++ src/navbar.jsx
@@ -0,0 +1,25 @@
+import './navbar.css'
+
+function NavigationBar(){
+    return(
+
+        <>
+            <nav>
+                <h1 id= "name">
+                    "Name"
+                </h1>
+                <div>
+                    <ul id= "navbar">
+                        <li> <a href="index.html">Home</a> </li>
+                        <li> <a href="index.html">Page1</a> </li>
+                       
+                    </ul>
+                </div>
+            </nav>
+        
+        
+        </>
+    )
+}
+
+export default NavigationBar
